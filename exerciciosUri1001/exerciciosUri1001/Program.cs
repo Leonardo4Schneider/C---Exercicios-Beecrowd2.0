9258929f8a08526cf6b1eeea0591760535acdb26
Leonardo4Schneider <leonardoschneider4096@gmail.com>
2025-04-03 21:56:30 -0300

    exercicio 1001 concluido

---
+++ exerciciosUri1001/exerciciosUri1001/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+int A, B, calculo1;
+A = int.Parse(Console.ReadLine());
+B = int.Parse(Console.ReadLine());
+
+calculo1 = A + B;
+Console.WriteLine("X = " + calculo1);
